de97a1f4214098688c3e0140233f45d0aeb1900f
Izdhan <57706976+Prof-Iz@users.noreply.github.com>
2020-06-11 05:43:51 +0800

    Update main.dart
    made code changes to connect to first serial device that is baud rate 9600

--- Android-Code/thermal_display/lib/main.dart
+++ Android-Code/thermal_display/lib/main.dart
@@ -6,48 +6,154 @@ import 'package:usb_serial/usb_serial.dart';
 import 'package:usb_serial/transaction.dart';
 
 void main() {
-  runApp(homeScreen());
+  runApp(HomeScreen());
 }
 
-class homeScreen extends StatefulWidget {
+class HomeScreen extends StatefulWidget {
   @override
-  _homeScreenState createState() => _homeScreenState();
+  _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _homeScreenState extends State<homeScreen> {
+class _HomeScreenState extends State<HomeScreen> {
+  UsbPort _port;
+  String _status = "Please Plug in the Device";
+  List<Widget> _ports = [];
+  String _serialData = "34.0";
+  StreamSubscription<String> _subscription;
+  Transaction<String> _transaction;
+  int _deviceId;
+
+  Future<bool> _connectTo(device) async {
+    _serialData = "INIT";
+
+    if (_subscription != null) {
+      _subscription.cancel();
+      _subscription = null;
+    }
+
+    if (_transaction != null) {
+      _transaction.dispose();
+      _transaction = null;
+    }
+
+//    if (_port != null) {
+//      _port.close();
+//      _port = null;
+//    }
+
+    if (device == null) {
+      _deviceId = null;
+      setState(() {
+        _status = "Disconnected";
+      });
+      return true;
+    }
+
+    _port = await device.create();
+    if (!await _port.open()) {
+      setState(() {
+        _status = "PORT Problem";
+      });
+      return false;
+    }
+
+    _deviceId = device.deviceId;
+    await _port.setDTR(true);
+    await _port.setRTS(true);
+    await _port.setPortParameters(9600, UsbPort.DATABITS_8, UsbPort.STOPBITS_1,
+        UsbPort.PARITY_NONE); //settings for Arduino Nano as per GitHub Repo
+
+    _transaction = Transaction.stringTerminated(
+        _port.inputStream, Uint8List.fromList([13, 10]));
+
+    _subscription = _transaction.stream.listen((String line) {
+      setState(() {
+        _serialData = line;
+      });
+    });
+
+    setState(() {
+      _status = "Connected";
+    });
+    return true;
+  }
+
+  void _getPorts() async {
+    _ports = [];
+    List<UsbDevice> devices = await UsbSerial.listDevices();
+    print(devices);
+
+//    devices.forEach((device) {
+//      _ports.add(ListTile(
+//          leading: Icon(Icons.usb),
+//          title: Text(device.productName),
+//          subtitle: Text(device.manufacturerName),
+//          trailing: RaisedButton(
+//            child:
+//            Text(_deviceId == device.deviceId ? "Disconnect" : "Connect"),
+//            onPressed: () {
+//              _connectTo(_deviceId == device.deviceId ? null : device)
+//                  .then((res) {
+//                _getPorts();
+//              });
+//            },
+//          )));
+
+//    });
+
+    devices.length > 0
+        ? _connectTo(devices[0])
+        : null; // connect to whatever is plugged into phone first
+
+    setState(() {
+      print(_ports);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    UsbSerial.usbEventStream.listen((UsbEvent event) {
+      _getPorts();
+    });
+
+    _getPorts();
+  }
+
+//  @override
+//  void dispose() {
+//    super.dispose();
+//    _connectTo(null);
+//  }
 
   Color alertColor = Colors.limeAccent;
-  String statusSerial = "Serial Connected";
-  String serialReading = "30.5";
   bool callHospital = false;
 
-  showHospital(){
+  requestTemperature() async {
+    await _port.write('G\r\n'.codeUnits);
+  }
+
+  showTemperature() {
+    requestTemperature();
     setState(() {
-      double temp = double.parse(serialReading);
-      temp+=0.3;
-      serialReading = temp.toStringAsFixed(1);
-      if (double.parse(serialReading) > 37.6) {
+      if (double.parse(_serialData) > 37.6) {
         callHospital = true;
         alertColor = Colors.redAccent;
       } else {
         callHospital = false;
-        if ((double.parse(serialReading) < 37.6) &&
-            (double.parse(serialReading) > 37.0)) {
+        if ((double.parse(_serialData) < 37.6) &&
+            (double.parse(_serialData) > 37.0)) {
           alertColor = Colors.orangeAccent;
-        }
-        else {
+        } else {
           alertColor = Colors.limeAccent;
         }
       }
-
-
     });
-
   }
+
   @override
   Widget build(BuildContext context) {
-    showHospital();
-
     return MaterialApp(
       title: "QE Thermal",
       debugShowCheckedModeBanner: false,
@@ -65,21 +171,26 @@ class _homeScreenState extends State<homeScreen> {
                   child: FloatingActionButton(
                     backgroundColor: Colors.white,
                     foregroundColor: Colors.redAccent,
-                    onPressed: ()=> MapsLauncher.launchQuery("hospitals near me"),
+                    onPressed: () =>
+                        MapsLauncher.launchQuery("hospitals near me"),
                     child: Icon(
                       Icons.local_hospital,
                     ),
                   ),
                 ),
-
-            ],),
+              ],
+            ),
           ),
           body: Padding(
-            padding: EdgeInsets.symmetric(vertical: 20.0,horizontal: 5.0),
+            padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 5.0),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
-              children: [Spacer(flex: 1,),
-                Row( //Serial Status
+              children: [
+                Spacer(
+                  flex: 1,
+                ),
+                Row(
+                  //Serial Status
                   children: [
                     Expanded(
                       child: FractionallySizedBox(
@@ -89,30 +200,43 @@ class _homeScreenState extends State<homeScreen> {
                             borderRadius: BorderRadius.circular(20.0),
                             color: Colors.black,
                           ),
-                          child: Text(statusSerial,
-                          style: TextStyle(
-                            fontSize: 14.0,
-                            color: Colors.white,
-                          ),textAlign: TextAlign.center,),
+                          child: Text(
+                            _status,
+                            style: TextStyle(
+                              fontSize: 14.0,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
                         ),
                       ),
                     )
                   ],
-                ),Spacer(flex: 1,),
-                Row( //Temperature Display Row
-                  children: [Expanded(
-                    child: FlatButton(
-                      splashColor: Colors.white10,
-                      onPressed: showHospital,
-                      child: Text(serialReading,
-                      style: TextStyle(
-                        fontSize: 150.0,
-                      ),
-                      textAlign: TextAlign.center,),
-                    ),
-                  )],
                 ),
-                Spacer(flex: 2,)
+                Spacer(
+                  flex: 1,
+                ),
+                Row(
+                  //Temperature Display Row
+                  children: [
+                    Expanded(
+                      child: FlatButton(
+                        splashColor: Colors.white10,
+                        onPressed: showTemperature,
+                        child: Text(
+                          _serialData,
+                          style: TextStyle(
+                            fontSize: 150.0,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+                Spacer(
+                  flex: 2,
+                )
               ],
             ),
           ),
